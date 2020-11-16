e22f89080056278ac35e8c9dd1193c2bbea718b9
Cintia Zago <cintiazago@gmail.com>
2020-11-16 18:51:35 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -34,7 +34,7 @@ export class SignUpController implements Controlller {
       })
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error.stack)
     }
   }
 }
