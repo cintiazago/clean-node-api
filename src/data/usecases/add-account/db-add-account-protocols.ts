dc2f60d2280724cbbbbd89d2b1ff447369102034
Cintia Zago <cintiazago@gmail.com>
2020-11-21 20:49:55 -0300

    refactor: rename Encrypter to Hasher

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
-export * from '../../protocols/criptograghy/encrypter'
+export * from '../../protocols/criptograghy/hasher'
 export * from '../../protocols/db/add-account-repository'
