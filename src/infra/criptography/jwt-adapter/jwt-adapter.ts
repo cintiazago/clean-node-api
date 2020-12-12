7f11a04f159c1924ecc4483a0e0e5a020c9000d3
Cintia Zago <cintiazago@gmail.com>
2020-12-12 16:02:28 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -8,4 +8,9 @@ export class JwtAdapter implements Encrypter {
     const accessToken = jwt.sign({ id: value }, this.secret)
     return accessToken
   }
+
+  async decrypt (value: string): Promise<string> {
+    await jwt.verify(value, this.secret)
+    return null
+  }
 }
