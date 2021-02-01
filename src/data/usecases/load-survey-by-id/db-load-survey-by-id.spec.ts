bbe1ad640b8f870ab99f2e6f4c6886c00c743485
Cintia Zago <cintiazago@gmail.com>
2021-02-01 19:48:23 -0300

    test: ensure DbLoadSurveyById throws if LoadSurveyByIdRepository throws

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -47,7 +47,7 @@ describe('DbLoadSurveysById', () => {
     MockDate.reset()
   })
 
-  test('Should call LoadSurveysRepository', async () => {
+  test('Should call LoadSurveyByIdRepository', async () => {
     const { sut, loadSurveyByIdRepositoryStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
     await sut.loadById('any_id')
@@ -58,5 +58,12 @@ describe('DbLoadSurveysById', () => {
     const { sut } = makeSut()
     const survey = await sut.loadById('any_id')
     expect(survey).toEqual(makeFakeSurvey())
+  })
+
+  test('Should throw if LoadSurveyByIdRepository throws', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.loadById('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
